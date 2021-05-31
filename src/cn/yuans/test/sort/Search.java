283667a8a28d07f8d888893c68f525afa4660f28
yuanshi <yuans321@163.com>
2021-05-31 21:30:49 +0800

    quicksort

--- src/cn/yuans/test/sort/Search.java
+++ src/cn/yuans/test/sort/Search.java
@@ -117,4 +117,43 @@ public class Search {
         }
         return -1;
     }
+
+
+    public static void quickSort(int[] arr, int start, int end) {
+        // 递归终止条件
+        if (start >= end) {
+            return;
+        }
+        // 第一步，找出分区后枢轴的下标，比如[2,1,3],枢轴为2，分区后枢轴的下标为1
+        int pivotIndex = partition(arr, start, end);
+        // 第二步，对左子数组排序
+        quickSort(arr, start, pivotIndex - 1);
+        // 第三步，对右子数组排序
+        quickSort(arr, pivotIndex + 1, end);
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[start];
+        int left = start;
+        int right = end;
+        while (left < right) {
+            //从右向左
+            while (left < right && arr[right] >= pivot) {
+                right--;
+            }
+            if (left < right) {
+                arr[left++] = arr[right];
+            }
+            //从左向右
+            while (left < right && arr[left] < pivot) {
+                left++;
+            }
+            if (left < right) {
+                arr[right--] = arr[left];
+            }
+        }
+        // 扫描完成后,将枢轴元素填入新坑中
+        arr[left] = pivot;
+        return left;
+    }
 }
